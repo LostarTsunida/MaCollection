1c2e7fdfbe6b101df6ac56c03772a78c987f8672
gmatteo <matteo.grange@ynov.com>
2026-09-17 23:23:24 +0200

    Mise en place token jwt

--- api/security.py
+++ api/security.py
@@ -1,9 +1,33 @@
-from passlib.context import Cryptcontext
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+import jwt
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # type: ignore
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password: str) -> str:
+def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+# --- JWT ---
+
+SECRET_KEY = "123456"  
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+
+    to_encode = data.copy()
+    
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        
+    
+    to_encode.update({"exp": expire})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
